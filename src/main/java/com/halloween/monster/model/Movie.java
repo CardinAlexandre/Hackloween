edbb35b1b0f2c8b2eaea8bf73a5bdbf9a9caa937
jerome valette <valettejerome31@gmail.com>
2019-10-30 15:31:09 +0100

    creation object movie

---
+++ src/main/java/com/halloween/monster/model/Movie.java
@@ -0,0 +1,69 @@
+package com.halloween.monster.model;
+
+public class Movie {
+    private int id;
+    private String title;
+    private String director;
+    private int year;
+    private String country;
+    private String createdAt;
+    private String updatedAt;
+
+    public Movie(int id) {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
