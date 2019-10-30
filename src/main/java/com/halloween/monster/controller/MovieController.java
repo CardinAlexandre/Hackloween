c8d1c87016a23a4d1ed006ef7679725923921861
PrieurA <alexandre-prieur_student2019@wilder.school>
2019-10-30 16:05:16 +0100

    movie controller

---
+++ src/main/java/com/halloween/monster/controller/MovieController.java
@@ -0,0 +1,50 @@
+package com.halloween.monster.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.halloween.monster.model.Movie;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Controller
+public class MovieController {
+
+    private static final String MOVIE_URL = "https://hackathon-wild-hackoween.herokuapp.com/";
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/movie")
+    public String planet(Model model, @RequestParam Long id) {
+
+        WebClient webClient = WebClient.create(MOVIE_URL);
+        Mono<String> call = webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/movies/{id}/")
+                        .build(id))
+                .retrieve()
+                .bodyToMono(String.class);
+
+        String response = call.block();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Movie movieObject = null;
+        // TODO : call the API and retrieve the planet
+
+        try {
+            movieObject = objectMapper.readValue(response, Movie.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        model.addAttribute("movieInfos", movieObject);
+
+        return "movie";
+    }
+}
